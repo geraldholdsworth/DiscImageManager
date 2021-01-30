1160a6de21ded8b3f21e249e953eb20fc7393f6d
Gerald Holdsworth <75666792+geraldholdsworth@users.noreply.github.com>
2021-01-30 10:47:59 +0000

    Update to 1.05.11

---
+++ LazarusSource/AboutUnit.pas
@@ -0,0 +1,37 @@
+unit AboutUnit;
+
+//This project is now covered by the GNU GPL v3 licence
+
+{$MODE objFPC}
+
+interface
+
+uses
+  SysUtils,Variants,Classes,Controls,Forms,Dialogs,ExtCtrls,StdCtrls;
+
+type
+
+  { TAboutForm }
+
+  TAboutForm = class(TForm)
+    CreditsPanel: TPanel;
+    Label15: TLabel;
+    Label16: TLabel;
+    lb_Title: TLabel;
+    Label8: TLabel;
+    lb_Version: TLabel;
+    Label14: TLabel;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  AboutForm: TAboutForm;
+
+implementation
+
+{$R *.lfm}
+
+end.
